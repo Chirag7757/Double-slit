3bf8ad9208a9cfb27a6b09aabbc2fcb8e11d5dd8
Chirag7757 <drphysicist775@gmail.com>
2025-02-24 08:37:09 +0530

    Add files via upload

--- Physics.py
+++ Physics.py
@@ -2,14 +2,16 @@ import streamlit as st
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-st.title("Double Slit Experiment")
+st.title("Double Slit Experiment Simulator")
+col1,col2=st.columns(2)
 
-d1= (st.slider("slit 1 and 2 width in cm", 0.01,0.1))/100
-d3= (st.slider("distnce between slit in cm", 0.05, 0.5))/100
-L=(st.slider("distance between screen and slit in cm", 5, 100))/100
-x=np.linspace(-L,L,50)
-la=st.slider("wavelength of photon",4000,7000)
-lamb=la*1e-10
+with col1:
+    d1= (st.slider("slit 1 and 2 width in cm", 0.01,0.1))/100
+    d3= (st.slider("distnce between slit in cm", 0.05, 0.5))/100
+    L=(st.slider("distance between screen and slit in cm", 5, 100))/100
+    x=np.linspace(-L,L,50)
+    la=st.slider("wavelength of photon",4000,7000)
+    lamb=la*1e-10
 
 psi1=np.sqrt(d1/(lamb*L))*np.sinc(np.pi*d1*x/(lamb*L))
 psi12=psi1**2
@@ -27,6 +29,5 @@ ax.set_ylabel("Intensity")
 ax.set_title("Double Slit Interference Pattern")
 ax.legend()
 ax.grid()
-
-
-st.pyplot(fig)
+with col2:
+    st.pyplot(fig)
