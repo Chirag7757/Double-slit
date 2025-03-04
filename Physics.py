947617532974eea4a92dd2e46b623968e5407c72
Chirag7757 <drphysicist775@gmail.com>
2025-03-04 21:07:24 +0530

    Update Physics.py

--- Physics.py
+++ Physics.py
@@ -1,8 +1,7 @@
 import streamlit as st 
 import numpy as np 
-import subprocess
-import sys
 import matplotlib.pyplot as plt
+import random
 
 
 st.title("Double Slit Experiment Simulator")
@@ -10,16 +9,31 @@ col1,col2=st.columns(2)
 d1= 0.0003
 d3= 0.0003
 L=2
-x=np.linspace(-L,L,50)
+x=np.linspace(-L,L,10000)
 lamb=0.00006
+list=[]
+n=0
+n1=0
+n2=0
+n_0=0
+n_1=0
+n_2=0
 
 with col1:
-    d1= (st.slider("slit 1 and 2 width in cm", 0.01,0.1))/100
-    d3= (st.slider("distnce between slit in cm", 0.05, 0.5))/100
-    L=(st.slider("distance between screen and slit in cm", 5, 100))/100
-    x=np.linspace(-L,L,50)
-    la=st.slider("wavelength of photon",4000,7000)
-    lamb=la*1e-10
+    if st.button("-100):
+                 n_2-=100
+    if st.button("-10):
+                 n_1-=10
+    if st.button("-1):
+                 n_0-=1
+    if st.button("+100):
+                 n1+=100
+    if st.button("+10):
+                 n2+=10
+    if st.button("+1):
+                 n+=1
+    
+N=n1+n=n2+n_1+n_0+n_2
 
 psi1=np.sqrt(d1/(lamb*L))*np.sinc(np.pi*d1*x/(lamb*L))
 psi12=psi1**2
@@ -28,9 +42,12 @@ psi22=psi2**2
 
 psi3=2*np.sqrt(d3/(lamb*L))*np.sinc(np.pi*d3*x/(lamb*L))
 y=abs(psi12+psi22+psi3)
+y2=y/np.sum(y)
+x1 = np.random.choice(x, size=N, p=y2)
+y3 = np.random.rand(N) * np.max(y)
 
 fig, ax = plt.subplots()
-ax.scatter(x, y, label="Interference Pattern")
+ax.scatter(x1,y3 label="Interference Pattern")
 ax.set_xlabel("Screen Position (m)")
 ax.set_ylabel("Intensity")
 ax.set_title("Double Slit Interference Pattern")
